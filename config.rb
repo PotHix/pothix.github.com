cb31cc7c77a3b3fb0fca93e91496e7a84fe3b26a
PotHix <pothix@pothix.com>
2015-06-15 09:42:48 -0300

    Removed useless comments.

--- config.rb
+++ config.rb
@@ -2,25 +2,16 @@
 # Blog settings
 ###
 
-# Time.zone = "UTC"
+Time.zone = "UTC"
 
 set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true, :smartypants => true
 
 activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
-
   blog.permalink = "{year}/{month}/{day}/{title}.html"
-  # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
   blog.taglink = "tags/{tag}.html"
   blog.layout = "post_layout"
-  # blog.summary_separator = /(READMORE)/
   blog.summary_length = 400
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
   blog.default_extension = ".markdown"
 
   blog.tag_template = "tag.html"
@@ -28,7 +19,6 @@ activate :blog do |blog|
 
   blog.sources = "posts/:title.html"
 
-  # Enable pagination
   blog.paginate = true
   blog.per_page = 10
   blog.page_link = "page/{num}"
@@ -55,44 +45,13 @@ page "/feed_tech.xml", layout: false
 # end
 
 ###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", layout: false
-#
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-###
 # Helpers
 ###
 
 activate :syntax, :line_numbers => true
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+end
 
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
